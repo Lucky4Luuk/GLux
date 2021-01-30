5bc710e6e84c75d449a8106608ac51e3ccb4b836
Luuk van Oijen <lazyluuk.channel@gmail.com>
2021-01-30 02:45:18 +0100

    Update texture.rs

--- src/gl_types/texture.rs
+++ src/gl_types/texture.rs
@@ -3,21 +3,29 @@ use core::ffi::c_void;
 /// 2D texture
 pub struct Texture {
     pub id: gl::types::GLuint,
-    pub format: gl::types::GLuint,
+    pub format: gl::types::GLint,
+    pub internal_format: gl::types::GLuint,
 }
 
 impl Texture {
-    pub fn new(size: (i32, i32), data: &[u8], format: gl::types::GLuint) -> Self {
+    pub fn new(size: (i32, i32), data: &[u8], format: gl::types::GLint, internal_format: gl::types::GLuint) -> Self {
+        Self::from_ptr(size, data.as_ptr() as *const c_void, format, internal_format)
+    }
+
+    pub fn from_ptr(size: (i32, i32), data: *const c_void, format: gl::types::GLint, internal_format: gl::types::GLuint) -> Self {
         let mut id: gl::types::GLuint = 0;
         unsafe {
             gl::GenTextures(1, &mut id);
             gl::BindTexture(gl::TEXTURE_2D, id);
-            gl::TexImage2D(gl::TEXTURE_2D, 0, format as i32, size.0, size.1, 0, format, gl::UNSIGNED_BYTE, data.as_ptr() as *const c_void);
+            gl::TexImage2D(gl::TEXTURE_2D, 0, format, size.0, size.1, 0, internal_format, gl::UNSIGNED_BYTE, data);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
             gl::BindTexture(gl::TEXTURE_2D, 0);
         }
         Self {
             id: id,
-            format: format
+            format: format,
+            internal_format: internal_format,
         }
     }
 
